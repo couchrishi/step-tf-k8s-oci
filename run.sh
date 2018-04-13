d528e0283119cc938bf7d65d8679f44ee1d38ab0
Mark Nelson <mark.x.nelson@oracle.com>
2018-04-12 21:19:36 -0400

    add empty run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Copyright 2018, Oracle Corporation and/or its affiliates. All rights reserved.
+
+info "This is the Wercker Step to Terraform a Kubernetes Cluster on Oracle Cloud Infrastructure."
+
+
